f9d4dfb743d2fa5de7786b09b943c083797e7927
sdpathak24 <202001258@daiict.ac.in>
2023-06-26 02:36:17 +0530

    textinput bug resolve

--- script.js
+++ script.js
@@ -90,95 +90,99 @@ textInput.addEventListener("input", () => {
 // Add event listener to save button
 if (saveButton) {
   saveButton.addEventListener("click", async function () {
-    let latestText = localStorage.getItem(currentDate);
+    if (textInput.value) {
+      let latestText = localStorage.getItem(currentDate);
 
-    // Create a new canvas element
-    const canvas = document.createElement("canvas");
-    const context = canvas.getContext("2d");
+      // Create a new canvas element
+      const canvas = document.createElement("canvas");
+      const context = canvas.getContext("2d");
 
-    // Set canvas dimensions to square with size 500px by 500px
-    canvas.width = 1000;
-    canvas.height = 1000;
+      // Set canvas dimensions to square with size 500px by 500px
+      canvas.width = 1000;
+      canvas.height = 1000;
 
-    // Set background color
-    context.fillStyle = getComputedStyle(textInput).backgroundColor;
-    context.fillRect(0, 0, canvas.width, canvas.height);
+      // Set background color
+      context.fillStyle = getComputedStyle(textInput).backgroundColor;
+      context.fillRect(0, 0, canvas.width, canvas.height);
 
-    // Set text style
-    const computedStyle = getComputedStyle(textInput);
-    context.fillStyle = computedStyle.color;
-    context.font = "100px " + computedStyle.fontFamily;
-    context.fontsize = computedStyle.fontSize;
-    context.textAlign = "center";
-    context.textBaseline = "middle";
-    context.direction = computedStyle.direction;
+      // Set text style
+      const computedStyle = getComputedStyle(textInput);
+      context.fillStyle = computedStyle.color;
+      context.font = "100px " + computedStyle.fontFamily;
+      context.fontsize = computedStyle.fontSize;
+      context.textAlign = "center";
+      context.textBaseline = "middle";
+      context.direction = computedStyle.direction;
 
-    // Draw text on canvas
-    const x = canvas.width / 2;
-    const y = canvas.height / 2;
-    const fontSize = parseInt(computedStyle.fontSize);
-    const lineHeight = fontSize * 1.5; // adjust line height as needed
-    const maxWidth = canvas.width - 100; // leave some margin on the sides
-    const lines = latestText.split("\n");
-    const maxLines = Math.floor(canvas.height / lineHeight);
-    if (lines.length > maxLines) {
-      lines.splice(maxLines, lines.length - maxLines);
-      lines[maxLines - 1] = lines[maxLines - 1].slice(0, -3) + "..."; // add ellipsis to last line if truncated
-    }
-    const textY = y - ((lines.length - 1) * lineHeight) / 2;
-    for (let i = 0; i < lines.length; i++) {
-      context.fillText(lines[i], x, textY + i * lineHeight);
-    }
+      // Draw text on canvas
+      const x = canvas.width / 2;
+      const y = canvas.height / 2;
+      const fontSize = parseInt(computedStyle.fontSize);
+      const lineHeight = fontSize * 1.5; // adjust line height as needed
+      const maxWidth = canvas.width - 100; // leave some margin on the sides
+      const lines = latestText.split("\n");
+      const maxLines = Math.floor(canvas.height / lineHeight);
+      if (lines.length > maxLines) {
+        lines.splice(maxLines, lines.length - maxLines);
+        lines[maxLines - 1] = lines[maxLines - 1].slice(0, -3) + "..."; // add ellipsis to last line if truncated
+      }
+      const textY = y - ((lines.length - 1) * lineHeight) / 2;
+      for (let i = 0; i < lines.length; i++) {
+        context.fillText(lines[i], x, textY + i * lineHeight);
+      }
 
-    // Add #HOTD text at the bottom of the canvas
-    // const hotdText = "#HOTD";
-    // possible update
-    const hotdText = `${currentDate} ` + "#HOTD";
-    const hotdFontSize = 50;
-    context.font = `${hotdFontSize}px` + computedStyle.fontFamily;
-    context.fillText(hotdText, x, canvas.height - hotdFontSize);
+      // Add #HOTD text at the bottom of the canvas
+      // const hotdText = "#HOTD";
+      // possible update
+      const hotdText = `${currentDate} ` + "#HOTD";
+      const hotdFontSize = 50;
+      context.font = `${hotdFontSize}px` + computedStyle.fontFamily;
+      context.fillText(hotdText, x, canvas.height - hotdFontSize);
 
-    // Convert canvas to image
-    // const image = canvas.toDataURL("image/png");
+      // Convert canvas to image
+      // const image = canvas.toDataURL("image/png");
 
-    // Save image
-    // const link = document.createElement("a");
-    // link.download = "myHOTD.png";
-    // link.href = image;
-    // link.click();
+      // Save image
+      // const link = document.createElement("a");
+      // link.download = "myHOTD.png";
+      // link.href = image;
+      // link.click();
 
-    // Convert canvas to image
-    const imageData = canvas.toDataURL("image/png");
+      // Convert canvas to image
+      const imageData = canvas.toDataURL("image/png");
 
-    // Create a Blob object from the Base64-encoded data
-    const byteString = atob(imageData.split(",")[1]);
-    const mimeType = imageData.split(",")[0].split(":")[1].split(";")[0];
-    const arrayBuffer = new ArrayBuffer(byteString.length);
-    const uint8Array = new Uint8Array(arrayBuffer);
-    for (let i = 0; i < byteString.length; i++) {
-      uint8Array[i] = byteString.charCodeAt(i);
-    }
-    const blob = new Blob([arrayBuffer], { type: mimeType });
+      // Create a Blob object from the Base64-encoded data
+      const byteString = atob(imageData.split(",")[1]);
+      const mimeType = imageData.split(",")[0].split(":")[1].split(";")[0];
+      const arrayBuffer = new ArrayBuffer(byteString.length);
+      const uint8Array = new Uint8Array(arrayBuffer);
+      for (let i = 0; i < byteString.length; i++) {
+        uint8Array[i] = byteString.charCodeAt(i);
+      }
+      const blob = new Blob([arrayBuffer], { type: mimeType });
 
-    // Share the Blob object
-    if (navigator.share) {
-      navigator
-        .share({
-          // title: "HOTD",
-          files: [new File([blob], "myHOTD.png", { type: "image/png" })],
-        })
-        .then(() => {
-          console.log("Thanks for sharing!");
-        })
-        .catch(console.error);
+      // Share the Blob object
+      if (navigator.share) {
+        navigator
+          .share({
+            // title: "HOTD",
+            files: [new File([blob], "myHOTD.png", { type: "image/png" })],
+          })
+          .then(() => {
+            console.log("Thanks for sharing!");
+          })
+          .catch(console.error);
+      } else {
+        alert(
+          "Sharing is not supported on this browser.\nDownloading image instead?"
+        );
+        const link = document.createElement("a");
+        link.download = "myHOTD.png";
+        link.href = imageData;
+        link.click();
+      }
     } else {
-      alert(
-        "Sharing is not supported on this browser.\nDownloading image instead?"
-      );
-      const link = document.createElement("a");
-      link.download = "myHOTD.png";
-      link.href = imageData;
-      link.click();
+      alert("Please enter some text");
     }
   });
 }
